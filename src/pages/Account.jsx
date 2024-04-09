be12508d0410e908439e03ef49f2f7215fd90212
olaf <juliendupdupdup@gmail.com>
2024-04-09 23:41:34 +0000

    component created

---
+++ src/pages/Account.jsx
@@ -0,0 +1,16 @@
+import AccountHistory from "../components/AccountHistory";
+
+
+export default function Account() {
+
+    
+
+  return (
+    
+    
+    <>
+      <AccountHistory/>
+    </>
+
+  );
+}
